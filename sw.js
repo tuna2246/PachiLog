f348fc29e3886954f7f36e7eb2e4d7f705a56233
Takuya2246 <tuna2246@gmail.com>
2026-08-20 16:59:03 +0900

    Update cache version from v2 to v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pachilog-v2";
+const CACHE = "pachilog-v3";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
