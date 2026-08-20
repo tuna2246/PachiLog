5fb3a2e9738f6b82e9068942391b53e0a8b9f421
Takuya2246 <tuna2246@gmail.com>
2026-08-20 17:43:31 +0900

    Add record editing and event trend filters
    Allow saved results to be edited and analyze trends by store and event.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pachilog-v4";
+const CACHE = "pachilog-v5";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
